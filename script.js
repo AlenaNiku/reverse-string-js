882f0c950dd4e7240fc4d6754cd0f230c4c94d8b
Alena Nikulina <alenanikulina0@gmail.com>
2020-09-13 20:28:34 -0400

    iterating with HOF forEach

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function reverseString(string) {
-    let reversedStr = ''
+    let reversedStr = '';
 
     for (let char of string) {
         reversedStr = char + reversedStr
@@ -8,3 +8,15 @@ function reverseString(string) {
 }
 
 reverseString("The quick brown fox")
+
+// To be able to use HOF such as .forEach() we need to turn our string into an array first
+// .forEach() takes in a funcrion (callback) that takes in an iterator as a parameter
+
+function reverseString2(string) {
+    let revString = '';
+
+    string.split('').forEach(element => {
+        revString = element + revString
+    });
+    return revString
+}
